d15ea7b0b3982ec0a5776035f5397dd84c71e98a
TgMrP <tgmrp.dev@gmail.com>
2021-04-19 19:32:30 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Meta from '../components/Meta';
+
+export default function Home() {
+  return (
+    <div>
+      <Meta title="About" />
+      <h1>About</h1>
+    </div>
+  );
+}
